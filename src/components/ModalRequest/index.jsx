a2138ac3f0b6eaf4401076f24ac63eee63c45223
DeltaPrice-Pedro <deltapricepedro@gmail.com>
2025-03-11 12:05:20 -0300

    Split ModalRequest

--- src/components/ModalRequest/index.jsx
+++ src/components/ModalRequest/index.jsx
@@ -1,12 +1,12 @@
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
-// import { FaCoins } from "react-icons/fa6";
+import { FaCoins } from "react-icons/fa6";
 
-export function ModalRequest(props){
+export function ModalRequest(props) {
     return (
         <Modal {...props} centered>
             <Modal.Header closeButton>
-                <Modal.Title id='title-modal'>Solicitar Pedido</Modal.Title>
+                <Modal.Title>{<FaCoins/>}<br/>Solicitar produto</Modal.Title>
             </Modal.Header>
             <Modal.Body>Confirma a escolha do produto abaixo?</Modal.Body>
             <Modal.Footer>
--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -7,12 +7,10 @@ import './styless.css';
 
 export const ProductCard = ({ cover, title, body }) => {
     const [show, setShow] = useState(false);
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
 
     return (
         <>
-            <div className="post" onClick={handleShow}>
+            <div className="post" onClick={() => setShow(true)}>
                 <img src={cover} alt={title} />
                 <div className="post-content">
                     <h2>
@@ -20,25 +18,11 @@ export const ProductCard = ({ cover, title, body }) => {
                     </h2>
                     <p>{body}</p>
                 </div>
-                {/* <ModalRequest
+            </div>
+            <ModalRequest
                 show={show}
                 onHide={() => setShow(false)}
-            /> */}
-            </div>
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleClose}>
-                        Close
-                    </Button>
-                    <Button variant="primary" onClick={handleClose}>
-                        Save Changes
-                    </Button>
-                </Modal.Footer>
-            </Modal>
+            />
         </>
     );
 }
