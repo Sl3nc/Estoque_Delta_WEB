8a85818f3128570d3b0228421bd53eb44806f278
DeltaPrice-Pedro <deltapricepedro@gmail.com>
2025-03-12 10:33:03 -0300

    modalRequest working

--- src/components/ModalRequest/index.jsx
+++ src/components/ModalRequest/index.jsx
@@ -1,37 +1,71 @@
+import { useEffect, useState } from 'react';
+
 import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
-import { FaDollarSign } from "react-icons/fa6";
-import { IconContext } from "react-icons";
+
 import { ProductSlim } from '../ProductSlim';
+
+import { FaCircleCheck, FaDollarSign } from "react-icons/fa6";
+import { IconContext } from "react-icons";
+
 import './styles.css'
 
 export function ModalRequest(props) {
+    const [requestBody, setRequestBody] = useState(true)
+
+    useEffect(() =>{
+        if (props.show == false){
+            setRequestBody(true)
+        }
+    })
+
+    const handleRequest = () => {
+        setRequestBody(!requestBody)
+    }
+
     return (
         <Modal {...props} centered>
             <Modal.Header closeButton>
                 <Modal.Title >
                     Solicitar produto
                 </Modal.Title>
-                <IconContext.Provider value={{ color: "#0597F2", size: "2em"}}>
+                <IconContext.Provider value={{ color: "#0597F2", size: "2em" }}>
                     {<FaDollarSign />}
                 </IconContext.Provider>
             </Modal.Header>
             <Modal.Body>
-                <p className='title-body'>Confirma a escolha do produto abaixo?</p>
-                {<ProductSlim
-                    cover={props.cover}
-                    title={props.title}
-                    body={props.body}
-                />
+                {requestBody &&
+                    <div>
+                        <p className='title-request'>Confirma a escolha do produto abaixo?</p>
+                        {<ProductSlim
+                            cover={props.cover}
+                            title={props.title}
+                            body={props.body}
+                        />
+                        }
+                    </div>
+                }
+                {requestBody == false &&
+                    <div>
+                        <p className='title-request'>Escolha do produto CONFIRMADA</p>
+                        <div className='content-request'>
+                            <IconContext.Provider value={{ color: "#0597F2", size: "4em" }}>
+                                <FaCircleCheck />
+                            </IconContext.Provider>
+                            <p className='warning-request'>Em caso de engano,<br />consulte a equipe responsável</p>
+                        </div>
+                    </div>
                 }
             </Modal.Body>
-            <Modal.Footer>
+            <Modal.Footer id='modal-footer'>
                 <Button variant="secondary" onClick={props.onHide}>
                     Fechar
                 </Button>
-                <Button variant="primary" onClick={props.onHide}>
-                    Confirmar
-                </Button>
+                {requestBody &&
+                    <Button variant="primary" onClick={handleRequest}>
+                        Confirmar
+                    </Button>
+                }
             </Modal.Footer>
         </Modal>
     )
