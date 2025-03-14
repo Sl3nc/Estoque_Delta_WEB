01ee35c3776a3216c6359d01ec194f948857ef79
DeltaPrice-Pedro <deltapricepedro@gmail.com>
2025-03-14 13:01:25 -0300

    deploy changes

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -21,7 +21,7 @@ export const Home = () => {
                 });
             }
         });
-    }, [navigate]);
+    }, []);
 
     useEffect(() => {
         handleUid();
--- src/templates/SignIn/index.jsx
+++ src/templates/SignIn/index.jsx
@@ -21,7 +21,7 @@ export const SignIn = () => {
         navigate('/')
       }
     });
-  }, [navigate]);
+  }, []);
 
   useEffect(() => {
     signInCheck();
