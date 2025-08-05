57abf0b20607b728d6c9751fad8516680cb6fead
Sl3nc <pedrohrm050315@hotmail.com>
2025-08-05 13:18:26 -0300

    filter amount

--- src/utils/products_firestore.js
+++ src/utils/products_firestore.js
@@ -1,12 +1,14 @@
 import { app } from './firebaseApp';
-import { getFirestore, getDocs, collection } from "firebase/firestore";
+import { getFirestore, getDocs, collection, query, where } from "firebase/firestore";
 
 export const products_firestore = async () => {
     const products = [];
     const db = getFirestore(app);
-    const querySnapshot = await getDocs(collection(db, "products"));
+    const querySnapshot = await getDocs(
+        query(collection(db, "products"), where("amount", ">", 0))
+    );
     querySnapshot.forEach((doc) => {
-        products.push({id: doc.id, ...doc.data()});
+        products.push({ id: doc.id, ...doc.data() });
     });
 
     return products
